2b6761a71c75525b1f5354aac258c36bb40e572f
Victoria Ukatu <victoria.ukatu@gmail.com>
2019-12-03 13:21:37 -0600

    Add SQL queries from Postico

--- database.sql
+++ database.sql
@@ -6,5 +6,28 @@
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+    "role" BOOLEAN NOT NULL
+);
+
+CREATE TABLE "kids_information" (
+	"id" SERIAL PRIMARY KEY,
+	"firstname" VARCHAR (80) NOT NULL,
+	"age" INT NOT NULL,
+	"picture" VARCHAR(300) NOT NULL,
+	"gender" VARCHAR NOT NULL,
+	"allergies" VARCHAR(300) NOT NULL,
+	"nap" VARCHAR NOT NULL,
+	"pottytrained" VARCHAR NOT NULL,
+	"notes" VARCHAR (500), 
+	"parentname" VARCHAR(100) NOT NULL,
+	"phone" VARCHAR(14) NOT NULL,
+	"user_id" INT REFERENCES "user"
+);
+
+CREATE TABLE "summaries" (
+	"id" SERIAL PRIMARY KEY,
+	"date" VARCHAR (30),
+	"summary" VARCHAR (500),
+	"user_id" INT REFERENCES "user"
 );
